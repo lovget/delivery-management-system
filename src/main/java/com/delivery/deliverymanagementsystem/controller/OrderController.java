d2323a65c1abe878b3b72c71d46f84edd269b6b2
lovget <lovgetios@gmail.com>
2026-02-25 15:01:17 +0300

    initial commit

--- src/main/java/com/delivery/deliverymanagementsystem/controller/OrderController.java
+++ src/main/java/com/delivery/deliverymanagementsystem/controller/OrderController.java
@@ -10,25 +10,24 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final OrderService orderService;
+private final OrderService orderService;
 
-    public OrderController(OrderService orderService) {
+public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
-    // GET с @PathVariable
     @GetMapping("/{id}")
     public OrderDto getOrderById(@PathVariable Long id) {
         return orderService.getOrderById(id);
     }
 
-    // GET с @RequestParam
+
     @GetMapping("/search")
     public List<OrderDto> getOrdersByStatus(@RequestParam String status) {
         return orderService.getOrdersByStatus(status);
     }
 
-    // Просто GET всех заказов
+
     @GetMapping
     public List<OrderDto> getAllOrders() {
         return orderService.getAllOrders();
