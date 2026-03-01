5c15fa2dd4757d0b2483d35b9bfe92712cf14d63
lovget <lovgetios@gmail.com>
2026-03-02 00:52:25 +0300

    point commit working v1.1

---
+++ src/main/java/com/delivery/deliverymanagementsystem/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.delivery.deliverymanagementsystem.controller;
+
+import com.delivery.deliverymanagementsystem.entity.Customer;
+import com.delivery.deliverymanagementsystem.service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public Customer create(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getAll() {
+        return customerService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getById(@PathVariable Long id) {
+        return customerService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Customer update(@PathVariable Long id, @RequestBody Customer customer) {
+        return customerService.update(id, customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        customerService.delete(id);
+    }
+}
