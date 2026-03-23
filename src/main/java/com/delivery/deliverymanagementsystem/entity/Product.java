c152e4b9b76313bed85dca2bd586a23bfb673d82
lovget <lovgetios@gmail.com>
2026-03-23 13:08:09 +0300

    point commit working v1.1

--- src/main/java/com/delivery/deliverymanagementsystem/entity/Product.java
+++ src/main/java/com/delivery/deliverymanagementsystem/entity/Product.java
@@ -1,6 +1,8 @@
 package com.delivery.deliverymanagementsystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
 import java.util.List;
 
 @Entity
@@ -15,43 +17,17 @@ public class Product {
 
     private double price;
 
-    @ManyToMany
-    @JoinTable(
-            name = "product_categories",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    private List<Category> categories;
-
     @ManyToMany(mappedBy = "products")
+    @JsonIgnore
     private List<Order> orders;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public List<Order> getOrders() { return orders; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public List<Category> getCategories() {
-        return categories;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
-    }
-
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setOrders(List<Order> orders) { this.orders = orders; }
 }
