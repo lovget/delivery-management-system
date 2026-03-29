99227b432d4cbb68a73468d7b7043bfd77228f7c
lovget <lovgetios@gmail.com>
2026-03-29 20:05:35 +0300

    point commit working v1.1

--- src/main/java/com/delivery/deliverymanagementsystem/entity/Payment.java
+++ src/main/java/com/delivery/deliverymanagementsystem/entity/Payment.java
@@ -18,8 +18,6 @@ public class Payment {
     @JsonIgnore
     private Order order;
 
-    private double amount;
-
     private String method;
 
     private LocalDateTime paidAt;
@@ -34,14 +32,6 @@ public class Payment {
 
     public void setOrder(Order order) {
         this.order = order;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
     }
 
     public String getMethod() {
