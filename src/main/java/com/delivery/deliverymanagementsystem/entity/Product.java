5812738988dce86f79f34bfe687c5e1864f3435a
lovget <lovgetios@gmail.com>
2026-03-30 14:51:07 +0300

    point commit working v1.1

--- src/main/java/com/delivery/deliverymanagementsystem/entity/Product.java
+++ src/main/java/com/delivery/deliverymanagementsystem/entity/Product.java
@@ -17,7 +17,7 @@ public class Product {
 
     private double price;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
             name = "product_categories",
             joinColumns = @JoinColumn(name = "product_id"),
