e5750a21988e8e8c0a274e0ed1f2367c971d083b
lovget <lovgetios@gmail.com>
2026-03-30 14:42:50 +0300

    point commit working v1.1

--- src/main/java/com/delivery/deliverymanagementsystem/service/ProductService.java
+++ src/main/java/com/delivery/deliverymanagementsystem/service/ProductService.java
@@ -1,21 +1,46 @@
 package com.delivery.deliverymanagementsystem.service;
 
+import com.delivery.deliverymanagementsystem.entity.Category;
 import com.delivery.deliverymanagementsystem.entity.Product;
+import com.delivery.deliverymanagementsystem.repository.CategoryRepository;
 import com.delivery.deliverymanagementsystem.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository,
+                          CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Product create(Product product) {
+
+        if (product.getCategories() != null) {
+            List<Category> resultCategories = new ArrayList<>();
+
+            for (Category c : product.getCategories()) {
+
+                Category category = categoryRepository.findByName(c.getName())
+                        .orElseGet(() -> {
+                            Category newCategory = new Category();
+                            newCategory.setName(c.getName());
+                            return categoryRepository.save(newCategory);
+                        });
+
+                resultCategories.add(category);
+            }
+
+            product.setCategories(resultCategories);
+        }
+
         return productRepository.save(product);
     }
 
